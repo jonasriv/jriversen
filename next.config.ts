b1c336bb1e90c0a6b22d4914ae829cb78e0cbea7
jonasriv <jonasriv@gmail.com>
2026-03-19 12:37:31 +0100

    added redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://jonasiversen.no/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
